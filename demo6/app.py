f2a1bc6ff6dc712e987e4ffab677abfb7c1d2e8c
henry.lee <henry.lee@quad.tw>
2023-06-05 10:26:52 +0800

    feat: Demo action + url_for.

--- demo6/app.py
+++ demo6/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import url_for, redirect, render_template
+from flask import url_for, redirect, render_template, request
 
 app = Flask(__name__)
 
@@ -30,3 +30,16 @@ def b():
     route = url_for('url_for_a')
     #  會將使用者引導到'/a'這個路由
     return redirect(route)
+
+@app.route('/login', methods=['GET', 'POST']) 
+def login():
+    #  利用request取得使用者端傳來的方法為何
+    if request.method == 'POST':
+                          #  利用request取得表單欄位值
+        return 'Hello ' + request.values['username']
+
+    # return "<form method='post' action='/login'><input type='text' name='username' />" \
+    #         "</br>" \
+    #        "<button type='submit'>Submit</button></form>"
+    #  非POST的時候就會回傳一個空白的模板
+    return render_template('login.html')
