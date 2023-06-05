0c0b47755191cb20108c9f30d3afbd6c344cf968
henry.lee <henry.lee@quad.tw>
2023-06-05 14:15:52 +0800

    feat: ORM one-to-many association.

--- demo7/app.py
+++ demo7/app.py
@@ -1,8 +1,6 @@
-from datetime import datetime
+from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from flask import Flask
- 
-db = SQLAlchemy()
+from datetime import datetime
  
 app = Flask(__name__)
 
@@ -20,19 +18,23 @@ app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
 app.config['SQLALCHEMY_ECHO'] = True
 
 # 初始化
-db.init_app(app)
+db = SQLAlchemy(app)
 
+# ==========================================================================================
 # 模型( model )定義
+# ==========================================================================================
 class Product(db.Model):
     __tablename__ = 'product'
     pid = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), unique=True, nullable=False)
     price = db.Column(db.Integer, nullable=False)
     img = db.Column(db.String(100), unique=True, nullable=False)
-    description = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.String(255), nullable=True)
     state = db.Column(db.String(10), nullable=False)
-    insert_time = db.Column(db.DateTime, default=datetime.now)
-    update_time = db.Column(db.DateTime, onupdate=datetime.now, default=datetime.now)
+    insert_time = db.Column(db.DateTime, default=datetime.now, nullable=False)
+    update_time = db.Column(db.DateTime, onupdate=datetime.now, default=datetime.now, nullable=False)
+ 
+    db_product_addtocar = db.relationship("AddToCar", backref="product")
  
     def __init__(self, name, price, img, description, state):
         self.name = name
@@ -40,22 +42,46 @@ class Product(db.Model):
         self.img = img
         self.description = description
         self.state = state
+ 
+class User(db.Model):
+    __tablename__ = 'user'
+    uid = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    role = db.Column(db.String(10), nullable=False)
+    insert_time = db.Column(db.DateTime, default=datetime.now, nullable=False)
+    update_time = db.Column(
+        db.DateTime, onupdate=datetime.now, default=datetime.now, nullable=False)
+ 
+    db_user_atc = db.relationship("AddToCar", backref="user")
+ 
+    def __init__(self, name, password, role):
+        self.name = name
+        self.password = password
+        self.role = role
+ 
+class AddToCar(db.Model):
+    __tablename__ = 'addtocar'
+    id = db.Column(db.Integer, primary_key=True)
+    quantity = db.Column(db.Integer, nullable=False)
+    state = db.Column(db.String(5), nullable=False)
+    insert_time = db.Column(db.DateTime, default=datetime.now, nullable=False)
+    update_time = db.Column(db.DateTime, onupdate=datetime.now, default=datetime.now, nullable=False) 
+    uid = db.Column(db.Integer, db.ForeignKey('user.uid'), nullable=False)
+    pid = db.Column(db.Integer, db.ForeignKey('product.pid'), nullable=False)
+ 
+    def __init__(self, uid, pid, quantity, state):
+        self.uid = uid
+        self.pid = pid
+        self.quantity = quantity
+        self.state = state
+# ==========================================================================================
 
 @app.route('/')
 def index():
     db.create_all()
-    # Read data
-    query = Product.query.filter_by(name='Max').first()
-    print("name:", query.name)
-    print("price:", query.price)
-    
-    # 可以用動態參數傳入
-    filters = {'name': 'Max', 'price': 8888}
-    query = Product.query.filter_by(**filters).first()
-    print("name:", query.name)
-    print("price:", query.price)
-
-    queries = Product.query.order_by(Product.price.desc())
-    for q in queries:
-        print(q.name, q.price)
+    # 查看
+    query = AddToCar.query.first()
+    print("query.product.name:", query.product.name)  
+    print("query.user.name:", query.user.name)  
     return 'ok'
