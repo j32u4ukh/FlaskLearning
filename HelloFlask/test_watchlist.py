911fc9de84deffe6a37321814c838faa97870f90
henry.lee <henry.lee@quad.tw>
2023-06-06 14:41:08 +0800

    test: Add unit test with unittest.

---
+++ HelloFlask/test_watchlist.py
@@ -0,0 +1,280 @@
+import unittest
+
+from app import Movie, User, app, db, forge, initdb
+
+
+class WatchlistTestCase(unittest.TestCase):
+
+    def setUp(self):
+        # 更新配置
+        app.config.update(
+            # 將 TESTING 設為 True 來開啟測試模式，這樣在出錯時不會輸出多余信息
+            TESTING = True,
+            # 將 SQLALCHEMY_DATABASE_URI 設為 'sqlite:///:memory:'，這會使用 SQLite 內存型數據庫，不會干擾開發時使用的數據庫文件。
+            # 也可以使用不同文件名的 SQLite 數據庫文件，但內存型數據庫速度更快。
+            SQLALCHEMY_DATABASE_URI='sqlite:///:memory:'
+        )
+        app.app_context().push()
+        # 創建數據庫和表
+        db.create_all()
+        # 創建測試數據，一個用戶，一個電影條目
+        user = User(name='Test', username='test')
+        user.set_password('123')
+        movie = Movie(title='Test Movie Title', year='2019')
+        # 使用 add_all() 方法一次添加多個模型類實例，傳入列表
+        db.session.add_all([user, movie])
+        db.session.commit()
+
+        # 返回一個測試客戶端對象，可以用來模擬客戶端（瀏覽器）
+        self.client = app.test_client()
+        
+        # 創建測試命令運行器
+        self.runner = app.test_cli_runner()  
+
+    def tearDown(self):
+        db.session.remove()  # 清除數據庫會話
+        db.drop_all()  # 刪除數據庫表
+
+    # 輔助方法，用於登入用戶
+    def login(self):
+        self.client.post('/login', data=dict(
+            username='test',
+            password='123'
+        ), follow_redirects=True)
+
+    # 測試程序實例是否存在
+    def test_app_exist(self):
+        self.assertIsNotNone(app)
+
+    # 測試程序是否處於測試模式
+    def test_app_is_testing(self):
+        self.assertTrue(app.config['TESTING'])
+
+    # 測試虛擬數據
+    def test_forge_command(self):
+        result = self.runner.invoke(forge)
+        self.assertIn('Done.', result.output)
+        self.assertNotEqual(Movie.query.count(), 0)
+
+    # 測試初始化數據庫
+    def test_initdb_command(self):
+        result = self.runner.invoke(initdb)
+        self.assertIn('Initialized database.', result.output)
+
+    # 測試生成管理員賬戶
+    def test_admin_command(self):
+        db.drop_all()
+        db.create_all()
+        result = self.runner.invoke(args=['admin', '--username', 'grey', '--password', '123'])
+        self.assertIn('Creating user...', result.output)
+        self.assertIn('Done.', result.output)
+        self.assertEqual(User.query.count(), 1)
+        self.assertEqual(User.query.first().username, 'grey')
+        self.assertTrue(User.query.first().validate_password('123'))
+
+    # 測試更新管理員賬戶
+    def test_admin_command_update(self):
+        # 使用 args 參數給出完整的命令參數列表
+        result = self.runner.invoke(args=['admin', '--username', 'peter', '--password', '456'])
+        self.assertIn('Updating user...', result.output)
+        self.assertIn('Done.', result.output)
+        self.assertEqual(User.query.count(), 1)
+        self.assertEqual(User.query.first().username, 'peter')
+        self.assertTrue(User.query.first().validate_password('456'))
+
+    # 測試 404 頁面
+    def test_404_page(self):
+        response = self.client.get('/nothing')  # 傳入目標 URL
+        data = response.get_data(as_text=True)
+        self.assertIn('Page Not Found - 404', data)
+        self.assertIn('Go Back', data)
+        self.assertEqual(response.status_code, 404)  # 判斷響應狀態碼
+
+    # 測試主頁
+    def test_index_page(self):
+        response = self.client.get('/')
+        data = response.get_data(as_text=True)
+        self.assertIn('Test\'s Watchlist', data)
+        self.assertIn('Test Movie Title', data)
+        self.assertEqual(response.status_code, 200)
+    
+    # 測試創建條目
+    def test_create_item(self):
+        self.login()
+
+        # 測試創建條目操作
+        response = self.client.post('/', data=dict(
+            title='New Movie',
+            year='2019'
+        ), follow_redirects=True)
+        data = response.get_data(as_text=True)
+        self.assertIn('Item created.', data)
+        self.assertIn('New Movie', data)
+
+        # 測試創建條目操作，但電影標題為空
+        response = self.client.post('/', data=dict(
+            title='',
+            year='2019'
+        ), follow_redirects=True)
+        data = response.get_data(as_text=True)
+        self.assertNotIn('Item created.', data)
+        self.assertIn('Invalid input.', data)
+
+        # 測試創建條目操作，但電影年份為空
+        response = self.client.post('/', data=dict(
+            title='New Movie',
+            year=''
+        ), follow_redirects=True)
+        data = response.get_data(as_text=True)
+        self.assertNotIn('Item created.', data)
+        self.assertIn('Invalid input.', data)
+
+    # 測試更新條目
+    def test_update_item(self):
+        self.login()
+
+        # 測試更新頁面
+        response = self.client.get('/movie/edit/1')
+        data = response.get_data(as_text=True)
+        self.assertIn('Edit item', data)
+        self.assertIn('Test Movie Title', data)
+        self.assertIn('2019', data)
+
+        # 測試更新條目操作
+        response = self.client.post('/movie/edit/1', data=dict(
+            title='New Movie Edited',
+            year='2019'
+        ), follow_redirects=True)
+        data = response.get_data(as_text=True)
+        self.assertIn('Item updated.', data)
+        self.assertIn('New Movie Edited', data)
+
+        # 測試更新條目操作，但電影標題為空
+        response = self.client.post('/movie/edit/1', data=dict(
+            title='',
+            year='2019'
+        ), follow_redirects=True)
+        data = response.get_data(as_text=True)
+        self.assertNotIn('Item updated.', data)
+        self.assertIn('Invalid input.', data)
+
+        # 測試更新條目操作，但電影年份為空
+        response = self.client.post('/movie/edit/1', data=dict(
+            title='New Movie Edited Again',
+            year=''
+        ), follow_redirects=True)
+        data = response.get_data(as_text=True)
+        self.assertNotIn('Item updated.', data)
+        self.assertNotIn('New Movie Edited Again', data)
+        self.assertIn('Invalid input.', data)
+
+    # 測試刪除條目
+    def test_delete_item(self):
+        self.login()
+
+        response = self.client.post('/movie/delete/1', follow_redirects=True)
+        data = response.get_data(as_text=True)
+        self.assertIn('Item deleted.', data)
+        self.assertNotIn('Test Movie Title', data)
+
+    # 測試登錄保護
+    def test_login_protect(self):
+        response = self.client.get('/')
+        data = response.get_data(as_text=True)
+        self.assertNotIn('Logout', data)
+        self.assertNotIn('Settings', data)
+        self.assertNotIn('<form method="post">', data)
+        self.assertNotIn('Delete', data)
+        self.assertNotIn('Edit', data)
+
+    # 測試登錄
+    def test_login(self):
+        response = self.client.post('/login', data=dict(
+            username='test',
+            password='123'
+        ), follow_redirects=True)
+        data = response.get_data(as_text=True)
+        self.assertIn('Login success.', data)
+        self.assertIn('Logout', data)
+        self.assertIn('Settings', data)
+        self.assertIn('Delete', data)
+        self.assertIn('Edit', data)
+        self.assertIn('<form method="post">', data)
+
+        # 測試使用錯誤的密碼登錄
+        response = self.client.post('/login', data=dict(
+            username='test',
+            password='456'
+        ), follow_redirects=True)
+        data = response.get_data(as_text=True)
+        self.assertNotIn('Login success.', data)
+        self.assertIn('Invalid username or password.', data)
+
+        # 測試使用錯誤的用戶名登錄
+        response = self.client.post('/login', data=dict(
+            username='wrong',
+            password='123'
+        ), follow_redirects=True)
+        data = response.get_data(as_text=True)
+        self.assertNotIn('Login success.', data)
+        self.assertIn('Invalid username or password.', data)
+
+        # 測試使用空用戶名登錄
+        response = self.client.post('/login', data=dict(
+            username='',
+            password='123'
+        ), follow_redirects=True)
+        data = response.get_data(as_text=True)
+        self.assertNotIn('Login success.', data)
+        self.assertIn('Invalid input.', data)
+
+        # 測試使用空密碼登錄
+        response = self.client.post('/login', data=dict(
+            username='test',
+            password=''
+        ), follow_redirects=True)
+        data = response.get_data(as_text=True)
+        self.assertNotIn('Login success.', data)
+        self.assertIn('Invalid input.', data)
+
+    # 測試登出
+    def test_logout(self):
+        self.login()
+
+        response = self.client.get('/logout', follow_redirects=True)
+        data = response.get_data(as_text=True)
+        self.assertIn('Goodbye.', data)
+        self.assertNotIn('Logout', data)
+        self.assertNotIn('Settings', data)
+        self.assertNotIn('Delete', data)
+        self.assertNotIn('Edit', data)
+        self.assertNotIn('<form method="post">', data)
+
+    # 測試設置
+    def test_settings(self):
+        self.login()
+
+        # 測試設置頁面
+        response = self.client.get('/settings')
+        data = response.get_data(as_text=True)
+        self.assertIn('Settings', data)
+        self.assertIn('Your Name', data)
+
+        # 測試更新設置
+        response = self.client.post('/settings', data=dict(
+            name='Grey Li',
+        ), follow_redirects=True)
+        data = response.get_data(as_text=True)
+        self.assertIn('Settings updated.', data)
+        self.assertIn('Grey Li', data)
+
+        # 測試更新設置，名稱為空
+        response = self.client.post('/settings', data=dict(
+            name='',
+        ), follow_redirects=True)
+        data = response.get_data(as_text=True)
+        self.assertNotIn('Settings updated.', data)
+        self.assertIn('Invalid input.', data)
+        
+if __name__ == '__main__':
+    unittest.main()
